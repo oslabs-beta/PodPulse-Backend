ebb6ba1489db707a8c11a656fda1aeebf971d99b
Daniel Mahoney <daniel.mahoney55@gmail.com>
2024-07-19 20:32:09 -0400

    removed dupe auth

--- server.js
+++ server.js
@@ -37,11 +37,6 @@ app.get('/auth', authcontroller.verify, (req, res) => {
   return res.status(200).json(res.locals.verification);
 });
 
-app.get('/auth', authcontroller.verify, (req, res) => {
-  console.log('made it out');
-  return res.status(200).json(res.locals.verification);
-});
-
 app.get(
   '/getNamespaceState/:username/:namespace/',
   dbController.getNamespaceState,
